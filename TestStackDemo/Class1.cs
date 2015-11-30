8b86313c406e0675c99dbeed143bc7729939da09
sumit6777 <shshba@bp1.ad.bp.com>
2015-11-30 11:31:07 +0000

    Updated Project with OWB Code

--- TestStackDemo/Class1.cs
+++ TestStackDemo/Class1.cs
@@ -2,11 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TabItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.AutomationElementSearch;
+using TestStack.White.Recording;
+using TestStack.White.UIItemEvents;
+using TestStack.White.UIItems.Actions;
+using TestStack.White.UIItems.Scrolling;
+//using System.Windows.Automation;
 
 namespace TestStackDemo
 {
-    public class Class1
+    class Class1
     {
+        private Window _window;
+
+        public Class1(Application app)
+        {
+            //_window = app.GetWindow("Operators WorkBench");
+            _window = app.GetWindow("Operators WorkBench");
+        }
+        public void Login()
+        {
+            _window.Get<TextBox>(SearchCriteria.ByAutomationId("txtUserName")).SetValue("guest2");
+            _window.Get<TextBox>(SearchCriteria.ByAutomationId("txtOldPassword")).SetValue("guest");
+            _window.Get<Button>(SearchCriteria.ByText("Login")).Click();
+            //_window.WaitWhileBusy();
+            Thread.Sleep(20000);
+        }
+
+        public bool isOnDashboard(string v)
+        {
+            // _window.WaitWhileBusy();
+            // _window.ReloadIfCached();
+            //_window.Get<Tab>(SearchCriteria.ByText("Bp.Owb.Desktop.Utility.AppMenuBarTab")).DoubleClick();
+            _window.RightClick();
+            bool val = _window.Exists<Button>(SearchCriteria.ByText("Show"));
+            _window.Get<TextBox>(SearchCriteria.ByAutomationId("Text")).SetValue("Sam");
+            _window.Get<Button>(SearchCriteria.ByText("Show")).Click();
+            return val;
+        }
     }
 }
---
+++ TestStackDemo/Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.Finders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+
+
+using System.Threading;
+using NUnit.Framework;
+
+namespace TestStackDemo
+{
+    [TestClass]
+    public class Class2
+    {
+        [TestMethod]
+        public void Can_Login()
+        {
+            ProcessStartInfo p = new ProcessStartInfo(@"C:\owb\OWB\Main\Source\Owb\BP.OWB.Container\bin\Debug\Bp.Owb.Desktop.exe");
+            p.WorkingDirectory = @"C:\owb\OWB\Main\Source\Owb\BP.OWB.Container\bin\Debug";
+            Application app = Application.AttachOrLaunch(p);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(app);
+            var owb = new Class1(app);
+            owb.Login();
+            bool dashboard = owb.isOnDashboard("tabControl");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(dashboard);
+
+        }
+    }
+}
+
